123c69236d14373cd913cc5a7c047050bb9f4a89
William Vallis <102640961+WillVallis@users.noreply.github.com>
2022-05-10 22:03:16 +1000

    Made some progress with serial module.
    Implemented basic serial module stuff. Struggling a little bit with clearing output buffer to make way for new message since system runs asynchronously.

---
+++ Code/Major Project/Sources/main.c
@@ -0,0 +1,81 @@
+#include <hidef.h>      /* common defines and macros */
+#include <stdlib.h>
+#include <string.h>
+#include "derivative.h"      /* derivative-specific definitions */
+
+#include "serial.h"
+
+
+const char UNKNOWN_COMMAND_TEMPLATE[] = "Error! Unknown command: _.\r";
+const char WELCOME_MESSAGE[] = "Hello! The device should now be tracking you! Feel free to enter a query!\n\
+To check distance between user and system, enter \'Distance\'\n\
+To check angle from datum, enter \'Angle\'\n\
+To check system status, enter \'Status\'\r";
+
+void printUnknownCommandError(SerialBuffer *serialBuffer, char command) {
+  // Create a buffer for the output message
+  char *msg = (char*)malloc(sizeof(UNKNOWN_COMMAND_TEMPLATE));
+  // Iterate over the template and copy the text over
+  int i = 0;
+  while (i < sizeof(UNKNOWN_COMMAND_TEMPLATE)) {
+    msg[i] = UNKNOWN_COMMAND_TEMPLATE[i];
+    i++;
+  }
+  // Replace the '_' with the command character
+  msg[24] = command;
+  // Send the message to the serial buffer
+  outputMessage(serialBuffer, msg);
+  // Free our msg buffer
+  free(msg);
+}
+
+// Our main loop
+void main(void) {
+  // Initialise the serial port and create a buffer object for it
+  SerialBuffer *sci1SerialBuffer = sci1Init(9600);
+
+  // Initialise our modules
+  initTimer();
+
+
+	EnableInterrupts;
+	
+	// Print welcome message
+	outputMessage(sci1SerialBuffer, WELCOME_MESSAGE);
+	// Clear output buffer
+  memset(sci1SerialBuffer,0,strlen(sci1SerialBuffer));
+	
+	
+  for(;;) { // Infinite loop
+    _FEED_COP(); 
+
+    if (sci1SerialBuffer->inputReady) { // Check if we have a new command
+      // User input is formatted as a single command character follwed by parameters
+      char *command = sci1SerialBuffer->inputString; // First character is our command
+      if (strcmp(command, "Distance\0") == 0) {
+        // Case for distance query 
+        // distance = getDistanceFunction();
+        // outputMessage(sci1SerialBuffer, distance);
+        outputMessage(sci1SerialBuffer, command);
+      }
+      if (strcmp(command, "Angle\0") == 0) {
+        // Case for angle query
+        // angle = getAngleFunction();
+        // outputMessage(sci1SerialBuffer, angle);
+        outputMessage(sci1SerialBuffer, command);
+      }
+      if (strcmp(command, "Status\0") == 0) {
+        // Case for status query 
+        // status = getStatusFunction();
+        // outputMessage(sci1SerialBuffer, status);
+        outputMessage(sci1SerialBuffer, command);
+      } 
+      else {
+          printUnknownCommandError(sci1SerialBuffer, *command);
+          break;
+      }
+      // We read the new message so mark as such
+      sci1SerialBuffer->inputReady = 0;
+    }
+  }
+}
---
+++ Code/Major Project/Sources/serial.h
@@ -0,0 +1,34 @@
+#ifndef SERIAL_HEADER
+#define SERIAL_HEADER
+
+
+#define BUFFER_SIZE 450 // How many chars the buffers in the SerialBuffer should be; high values like 512 break for some reason, 450 is seemingly safe
+#define TERMINATING_CHAR_INPUT '\r' // Special character used to determine the end of input
+#define TERMINATING_CHAR_OUTPUT '\0' // Same for output, *really* should be \0 as that's what strings end with
+
+// Data container for serial data i/o
+typedef struct SerialBuffer {
+  char portID; // Which SCI port this is for, unused for this but could theoretically be queried to check what port it's for
+  char *inputBuffer; // Array buffer for input
+  char *outputBuffer; // Array buffer for output
+
+  int inputWriteIndex; // Index for where in the buffer to write to
+  int outputWriteIndex; // Index for where in the buffer it is reading for during output
+
+  int inputReady;  // Flag for if the buffer is ready to be read, read by user
+  int outputReady; // Flag for if the buffer is ready to be sent, set by user
+
+  char echoChar; // Character to echo back into the terminal
+  
+  char *inputString; // Copy of input buffer for safe(ish) use with commands
+} SerialBuffer;                  
+
+void outputMessage(SerialBuffer *serialBuffer, char *msg);
+
+// Support for other ports need their own init and interrupt functions as they have different control addresses and potentially different flags.
+
+SerialBuffer *sci1Init(int baud_rate);
+
+__interrupt void sci1Interrupt(void);
+
+#endif
